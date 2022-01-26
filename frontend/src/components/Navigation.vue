95c4869c06e6b7d1b0d4aa4d2fa49203af1af5d5
James Scherer <jscherer@uab.edu>
2022-01-26 11:14:18 -0600

    Made a very important change

--- frontend/src/components/Navigation.vue
+++ frontend/src/components/Navigation.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="md:w-40 text-gray-700 bg-green-600">
     <div class="px-8 py-4 items-center justify-between">
-      <router-link to="/" class="text-lg font-semibold tracking-widest text-gray-900 uppercase rounded-lg focus:outline-none focus:shadow-outline">Delphi</router-link>
+      <router-link to="/" class="text-lg font-semibold tracking-widest text-gray-900 uppercase rounded-lg focus:outline-none focus:shadow-outline">diverGen</router-link>
       <button class="rounded-lg md:hidden focus:outline-none focus:shadow-outline">
         <svg fill="currentColor" viewBox="0 0 20 20" class="w-6 h-6">
           <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z" clip-rule="evenodd"></path>
