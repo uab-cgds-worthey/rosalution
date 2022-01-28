0ff192f7f9d4479799783e3c5ee3c59a26f95d60
James Scherer <jscherer@uab.edu>
2022-01-28 15:11:32 -0600

    Adding the first passing test and removing unused code

--- frontend/src/components/FormComponents/DropdownMenu.vue
+++
@@ -1,1 +0,0 @@
-// This file is where the Dropdown menu Vue component will be created
--- frontend/test/App.spec.js
+++ frontend/test/App.spec.js
@@ -1,12 +1,18 @@
 import {test, expect} from 'vitest';
+import {mount} from '@vue/test-utils';
 
-import {shallowMount} from '@vue/test-utils';
-import AnalysisListing from '../src/views/AnalysisListing.vue';
+import App from '../src/App.vue';
+// import AnalysisListing from '../src/views/AnalysisListing.vue';
 
-test('renders props.msg when passed', () => {
-  const msg = 'Listing of Analyses';
-  const wrapper = shallowMount(AnalysisListing, {
-    props: {msg},
-  });
-  expect(wrapper.html()).to.include(msg);
+test('Vue instance exists and it is an object', () => {
+  const wrapper = mount(App, { shallow: true });
+  expect(typeof wrapper).toBe('object');
 });
+
+// test('renders props.msg when passed', () => {
+//   const msg = 'Listing of Analyses';
+//   const wrapper = shallowMount(AnalysisListing, {
+//     props: {msg},
+//   });
+//   expect(wrapper.html()).to.include(msg);
+// });
