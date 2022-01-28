8ed74f02c1f3df76ad0838b2f437b0823ae39a1c
James Scherer <jscherer@uab.edu>
2022-01-28 13:21:59 -0600

    Added google rules and removed babel/no-invalid-this rule

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -4,13 +4,13 @@ module.exports = {
     },
     extends: [
       'eslint:recommended',
-      'plugin:vue/vue3-recommended',
+      'plugin:vue/vue3-essential',
+      'google'
     ],
     rules: {
         'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
         'max-len': [2, 120, 4],
-        'babel/no-invalid-this': 1,
         'no-invalid-this': 0,
     },
     ignorePatterns: [
