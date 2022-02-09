58129075a810a2f8605e7276a90f9d108338d9f9
James Scherer <jscherer@uab.edu>
2022-02-09 13:39:49 -0600

    Added the router to the NavigationComponent and updated the test

--- frontend/test/components/NavigationComponent.spec.js
+++ frontend/test/components/NavigationComponent.spec.js
@@ -1,15 +1,48 @@
-import {test, expect} from 'vitest';
+import {expect, describe, beforeAll, it} from 'vitest';
+import {createRouter, createWebHistory} from 'vue-router';
 import {mount} from '@vue/test-utils';
 
 import NavigationComponent from '../../src/components/NavigationComponent.vue';
+import AnalysisListingView from '../../src/views/AnalysisListingView.vue';
+import AnalysisCreateView from '../../src/views/AnalysisCreateView.vue';
+import AboutView from '../../src/views/AboutView.vue';
 
-// Skipping these tests due to the use of Vue Router.
-// Running test without mocking result in following warnings:
-// [Vue warn]: Failed to resolve component: router-view
-// [Vue warn]: Failed to resolve component: router-link
-// https://www.wrike.com/open.htm?id=837264942
+describe('NavigationComponent.vue', () => {
+  let wrapper;
 
-test.skip('Vue instance exists and it is an object', () => {
-  const wrapper = mount(NavigationComponent, {shallow: true});
-  expect(typeof wrapper).toBe('object');
+  const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+      {
+        path: '/',
+        component: AnalysisListingView,
+      },
+      {
+        path: '/analysis/create',
+        component: AnalysisCreateView,
+      },
+      {
+        path: '/about',
+        component: AboutView,
+      },
+    ],
+  });
+
+  beforeAll(() => {
+    wrapper = mount(NavigationComponent, {
+      global: {
+        plugins: [router],
+      },
+    });
+  });
+
+  it('Vue instance exists and it is an object', () => {
+    expect(typeof wrapper).toBe('object');
+  });
+
+  it('Navigation bar has 1 nav tag', () => {
+    const navTag = wrapper.findAll('nav');
+    // Expecting only a single nav tag. Update when links are added.
+    expect(navTag.length).toBe(1);
+  });
 });
