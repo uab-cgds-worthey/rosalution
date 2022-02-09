cf9f2069b2a92829793c4ce53015cb6e375db4af
James Scherer <jscherer@uab.edu>
2022-02-09 09:58:57 -0600

    Using a real router for testing to get rid of warnings for router-link and router-view tags

--- frontend/test/App.spec.js
+++ frontend/test/App.spec.js
@@ -1,6 +1,5 @@
 import {test, expect} from 'vitest';
 import {mount} from '@vue/test-utils';
-import sinon from 'sinon';
 
 import {createRouter, createWebHistory} from 'vue-router';
 
@@ -10,31 +9,7 @@ import AnalysisListingView from '../src/views/AnalysisListingView.vue';
 import AnalysisCreateView from '../src/views/AnalysisCreateView.vue';
 import AboutView from '../src/views/AboutView.vue';
 
-// Skipping these tests due to the use of Vue Router.
-// Running test without mocking result in following warnings:
-// [Vue warn]: Failed to resolve component: router-view
-// [Vue warn]: Failed to resolve component: router-link
-// https://www.wrike.com/open.htm?id=837264942
-test.skip('Vue instance exists and it is an object', () => {
-  const mockRouter = {
-    push: sinon.stub(),
-  };
-
-  const wrapper = mount(App, {
-    shallow: false,
-    global: {
-      mocks: {
-        $router: mockRouter,
-      },
-    },
-  });
-
-  console.log(wrapper);
-
-  expect(1).toBe(1);
-});
-
-test.only('Using a real router!', () => {
+test('Contains a <sidebar> tag with a <navigation> component', () => {
   const router = createRouter({
     history: createWebHistory(),
     routes: [
@@ -53,19 +28,11 @@ test.only('Using a real router!', () => {
     ],
   });
 
-  console.log(router);
-
   const wrapper = mount(App, {
     global: {
       plugins: [router],
     },
   });
-
-  console.log(wrapper.html());
-});
-
-test.skip('Contains a <sidebar> tag with a <navigation> component', () => {
-  const wrapper = mount(App, {shallow: true});
 
   expect(wrapper.find('app-sidebar').exists()).toBe(true);
 
