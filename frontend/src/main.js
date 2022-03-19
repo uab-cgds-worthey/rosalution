37f1eaae5a4098732a4489614c7279da077b40c9
Angelina Uno-Antonison <aunoantonison@uab.edu>
2022-03-19 08:21:44 -0500

    Created new yml for production since it needs a different traefik configuration; added configuration for nginx to support the vuejs router webhistory instead of webhash; created a base not found component to route 404 errors to; added documentation on how to handle local development

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,5 @@
 import {createApp} from 'vue';
-import {createRouter, createWebHashHistory} from 'vue-router';
+import {createRouter, createWebHistory} from 'vue-router';
 
 import App from './App.vue';
 import AnalysisListingView from './views/AnalysisListingView.vue';
