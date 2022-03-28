ab3cdab2e53685a82623df095e87c2e078ce9392
SeriousHorncat <ange.unoantonison@gmail.com>
2022-03-28 12:45:56 -0500

    Re-added the App.spec.js test, should not have removed.  Updated the test to be reflective of what the App.spec.js is doing for the meantime.

---
+++ frontend/test/App.spec.js
@@ -0,0 +1,41 @@
+import {test, expect} from 'vitest';
+import {shallowMount} from '@vue/test-utils';
+
+import {createRouter, createWebHistory} from 'vue-router';
+
+import App from '../src/App.vue';
+import AnalysisListingView from '../src/views/AnalysisListingView.vue';
+import AnalysisCreateView from '../src/views/AnalysisCreateView.vue';
+import AboutView from '../src/views/AboutView.vue';
+
+test('Contains a the <router-view> to display the application with routes', () => {
+  const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+      {
+        path: '/',
+        component: AnalysisListingView,
+      },
+      {
+        path: '/analysis/create',
+        component: AnalysisCreateView,
+      },
+      {
+        path: '/about',
+        component: AboutView,
+      },
+    ],
+  });
+
+  const wrapper = shallowMount(App, {
+    global: {
+      plugins: [router],
+      stubs: {
+        NavigationComponent: true,
+      },
+    },
+  });
+
+  console.log(wrapper.html());
+  expect(wrapper.find('router-view-stub').exists()).toBe(true);
+});
