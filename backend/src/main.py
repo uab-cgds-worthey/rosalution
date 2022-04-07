49b8a86465d9e83631b329bcf64774e5dac297d2
Rabab Fatima <rfatima@peds.uab.edu>
2022-04-07 16:52:35 -0500

    Added endpoints for Cases CPAM0002, CPAM0046, CPAM0053

--- backend/src/main.py
+++ backend/src/main.py
@@ -18,12 +18,78 @@ async def get_analysis_listing():
     # Better variable names need to be devised.
     path_to_current_file = os.path.realpath(__file__)
     current_directory = os.path.split(path_to_current_file)[0]
-    path_to_file = os.path.join(current_directory, "../models/analyses.json")
+    path_to_file = os.path.join(current_directory, "../fixtures/analyses.json")
     with open(path_to_file, mode='r', encoding='utf-8') as file_to_open:
         data = json.load(file_to_open)
         file_to_open.close()
 
     return data
+
+
+@app.get('/analysis/CPAM0002')
+async def get_analysis_by_id():
+    """ Returns analysis by id"""
+    # This is necessary for pytest to get relative pathing.
+    # Better variable names need to be devised.
+    path_to_current_file = os.path.realpath(__file__)
+    current_directory = os.path.split(path_to_current_file)[0]
+    path_to_file = os.path.join(current_directory, "../fixtures/analysis-data.json")
+    with open(path_to_file, mode='r', encoding='utf-8') as file_to_open:
+        analyses = json.load(file_to_open)
+        file_to_open.close()
+    
+    for analysis in analyses:
+        analysis_id = analysis.get('id')
+        if(analysis_id == "160d1134-784d-42f0-a839-7a88ea26f530"):
+            # Finding Case CPAM0053
+            result = analysis
+            break
+
+    return result
+
+
+@app.get('/analysis/CPAM0046')
+async def get_analysis_by_id():
+    """ Returns analysis by id"""
+    # This is necessary for pytest to get relative pathing.
+    # Better variable names need to be devised.
+    path_to_current_file = os.path.realpath(__file__)
+    current_directory = os.path.split(path_to_current_file)[0]
+    path_to_file = os.path.join(current_directory, "../fixtures/analysis-data.json")
+    with open(path_to_file, mode='r', encoding='utf-8') as file_to_open:
+        analyses = json.load(file_to_open)
+        file_to_open.close()
+    
+    for analysis in analyses:
+        analysis_id = analysis.get('id')
+        if(analysis_id == "10f7aa04-6adf-4538-a700-ebe2f519473f"):
+            # Finding Case CPAM0053
+            result = analysis
+            break
+
+    return result
+
+
+@app.get('/analysis/CPAM0053')
+async def get_analysis_by_id():
+    """ Returns analysis by id"""
+    # This is necessary for pytest to get relative pathing.
+    # Better variable names need to be devised.
+    path_to_current_file = os.path.realpath(__file__)
+    current_directory = os.path.split(path_to_current_file)[0]
+    path_to_file = os.path.join(current_directory, "../fixtures/analysis-data.json")
+    with open(path_to_file, mode='r', encoding='utf-8') as file_to_open:
+        analyses = json.load(file_to_open)
+        file_to_open.close()
+    
+    for analysis in analyses:
+        analysis_id = analysis.get('id')
+        if(analysis_id == "10342gs4-6adf-4538-a700-ebef319473f"):
+            # Finding Case CPAM0053
+            result = analysis
+            break
+
+    return result
 
 ## Test endpoints
 
