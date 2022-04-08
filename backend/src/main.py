66041a75d2404dc3d26de974b5528b69ee5cadfc
Rabab Fatima <rfatima@peds.uab.edu>
2022-04-08 15:26:34 -0500

    Restructured analysis-data.json to add sections, refactored main.py. Linted.

--- backend/src/main.py
+++ backend/src/main.py
@@ -5,7 +5,7 @@ import os
 import json
 import uvicorn
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
@@ -26,10 +26,8 @@ async def get_analysis_listing():
     return data
 
 
-def get_analysis_by_id(id_to_get):
+def find_analysis_by_name(name: str):
     """ Returns analysis by searching for id"""
-    # This is necessary for pytest to get relative pathing.
-    # Better variable names need to be devised.
     path_to_current_file = os.path.realpath(__file__)
     current_directory = os.path.split(path_to_current_file)[0]
     path_to_file = os.path.join(current_directory, "../fixtures/analysis-data.json")
@@ -37,28 +35,22 @@ def get_analysis_by_id(id_to_get):
         analyses = json.load(file_to_open)
         file_to_open.close()
     for analysis in analyses:
-        analysis_id = analysis.get('id')
-        if analysis_id == id_to_get:
-            # Finding Case corresponding to 'id_to_get'
-            data = analysis
-            break
+        analysis_name = analysis.get('name')
+        if analysis_name == name:
+            return analysis
 
-    return data
+    return None
 
-@app.get('/analysis/CPAM0002')
-async def get_case_two():
-    """ Returns CPAM0002 case data by calling method to find case by it's ID"""
-    return get_analysis_by_id("160d1134-784d-42f0-a839-7a88ea26f530")
-
-@app.get('/analysis/CPAM0046')
-async def get_case_forty_six():
-    """ Returns CPAM0046 case data by calling method to find case by it's ID"""
-    return get_analysis_by_id("10f7aa04-6adf-4538-a700-ebe2f519473f")
-
-@app.get('/analysis/CPAM0053')
-async def get_case_fifty_three():
-    """ Returns CPAM0053 case data by calling method to find case by it's ID"""
-    return get_analysis_by_id("10342gs4-6adf-4538-a700-ebef319473f")
+@app.get('/analysis/{name}')
+async def get_analysis_by_name(name: str):
+    """ Returns analysis case data by calling method to find case by it's name"""
+    if name == 'CPAM0002':
+        return find_analysis_by_name("CPAM0002")
+    if name == 'CPAM0046':
+        return find_analysis_by_name("CPAM0046")
+    if name == 'CPAM0053':
+        return find_analysis_by_name("CPAM0053")
+    raise HTTPException(status_code=404, detail="Item not found")
 
 ## Test endpoints
 
