d14f1b2ebd41fbfa648af26e3dfbb4e3f34420a3
James Scherer <jscherer@uab.edu>
2022-04-26 09:06:09 -0500

    Removed the duplicate code between the backend and the prototype endpoints for linting

--- backend/tests/integration/test_main.py
+++ backend/tests/integration/test_main.py
@@ -13,11 +13,3 @@ def test_get_analysis_listing():
     assert response.status_code == 200
     assert len(response.json()) == 4
     assert response.json()[2]['name'] == 'CPAM0053'
-
-## Tests for helper endpoints
-
-def test_read_main():
-    """Ensures Hello World is returned"""
-    response = client.get("/default")
-    assert response.status_code == 200
-    assert response.json() == {"Hello": "World"}
