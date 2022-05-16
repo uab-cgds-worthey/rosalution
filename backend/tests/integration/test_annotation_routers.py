f0c2ef3ca57bebeda1272e78229eaf756f712937
SeriousHorncat <ange.unoantonison@gmail.com>
2022-05-16 09:12:31 -0500

    Migrated the test_main.py to have the individual routers contain their own set of tests to help keep integration testing as clean and easy to follow as possible.  Utilizing the conftest.py to have universal fixtures needed for unit tests available for integration tests, (example is the FastApi client used to mock the client).

---
+++ backend/tests/integration/test_annotation_routers.py
@@ -0,0 +1,17 @@
+"""Routes dedicated for annotation within the system"""
+from unittest.mock import patch
+from fastapi import BackgroundTasks
+
+from src.annotation import AnnotationService
+
+def test_queue_annotations_for_sample(client, mock_annotation_queue):
+    """Testing that the correct number of analyses were returned and in the right order"""
+    # Future Database Mock Example
+    # mock_database_collections.db['analysis'].find()
+    # mock_database_collections.db['analysis'].find.return_value = JSON Fixture
+    with patch.object(BackgroundTasks, 'add_task', return_value=None) as mock_background_add_task:
+        response = client.post('/annotate/CPAM0002')
+        assert response.status_code == 202
+        assert mock_annotation_queue.put.call_count == 28
+        mock_background_add_task.assert_called_once_with(
+            AnnotationService.process_tasks, mock_annotation_queue)
--- backend/tests/integration/test_auth_routers.py
+++ backend/tests/integration/test_auth_routers.py
@@ -1,18 +1,8 @@
-"""Endpoint Integration for Analysis Routes"""
+"""Authentication Routes Intergration test"""
 import json
 from base64 import b64encode
-from unittest.mock import Mock, patch
-import pytest
-from fastapi.testclient import TestClient
-from fastapi import BackgroundTasks
 from cas import CASClient
 from itsdangerous import TimestampSigner
-
-from src.main import app
-from src.annotation import AnnotationService
-from src.database import Database
-
-from src.dependencies import database, annotation_queue
 
 # Helper functions
 
@@ -21,39 +11,6 @@ def create_session_cookie(data) -> str:
     signer = TimestampSigner(str('!secret'))
 
     return signer.sign(b64encode(json.dumps(data).encode('utf-8')),).decode('utf-8')
-
-## Tests for diverGen endpoints ##
-
-# Analyses Tests #
-
-def test_get_analyses(client):
-    """Testing that the correct number of analyses were returned and in the right order"""
-    # database_collections['analysis'].find()
-    # database_collections['analysis'].find.return_value = {'trash': 'json'}
-
-    response = client.get('/analysis/')
-    assert response.status_code == 200
-    assert len(response.json()) == 3
-    assert response.json()[2]['name'] == 'CPAM0053'
-
-
-def test_get_analysis_summary(client):
-    """Testing if the analysis summary endpoint returns all of the analyses available"""
-    response = client.get('/analysis/summary')
-    assert len(response.json()) == 5
-
-
-def test_queue_annotations_for_sample(client, mock_annotation_queue):
-    """Testing that the correct number of analyses were returned and in the right order"""
-    # Future Database Mock Example
-    # mock_database_collections.db['analysis'].find()
-    # mock_database_collections.db['analysis'].find.return_value = JSON Fixture
-    with patch.object(BackgroundTasks, 'add_task', return_value=None) as mock_background_add_task:
-        response = client.post('/annotate/CPAM0002')
-        assert response.status_code == 202
-        assert mock_annotation_queue.put.call_count == 28
-        mock_background_add_task.assert_called_once_with(
-            AnnotationService.process_tasks, mock_annotation_queue)
 
 # Authentication Tests #
 
@@ -111,31 +68,3 @@ def test_logout(client):
     response = client.get('/auth/logout')
     assert response.json()['url'] == 'https://padlockdev.idm.uab.edu/cas/logout?' \
                                      'service=http%3A%2F%2Ftestserver%2Fdivergen%2Fapi%2Fauth%2Flogin'
-
-## Tests fixtures ##
-
-@pytest.fixture(name='client', scope='class')
-def test_application_client():
-    """A class scoped FastApi Test Client"""
-    return TestClient(app)
-
-
-@pytest.fixture(name='mock_annotation_queue', scope='class')
-def mock_queue():
-    """A mocked Python queue used to verify if annotation tasks are created"""
-    annotation_queue.annotation_queue = Mock()
-    return annotation_queue.annotation_queue
-
-
-@pytest.fixture(name='database_collections', scope='class')
-def mock_database_collections():
-    """A mocked database client which overrides the database depedency injected """
-    mock_database_client = Mock()
-    mock_database_client.db = {
-        'analysis': Mock(),
-        'annotation': Mock()
-    }
-    mock_database = Database(mock_database_client)
-    app.dependency_overrides[database] = mock_database
-    yield mock_database_client.db
-    app.dependency_overrides.clear()
