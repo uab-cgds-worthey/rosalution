e33aa694460c3eeb605bebc9da1ef8e059006ec3
James Scherer <jscherer@uab.edu>
2022-05-27 09:20:33 -0500

    applying suggestion - removing console log

--- frontend/src/views/AnalysisView.vue
+++ frontend/src/views/AnalysisView.vue
@@ -39,7 +39,6 @@ export default {
       this.getSections();
     },
     getSections() {
-      console.log('pushing sections');
       this.sectionsList=this.analysis.sections;
       console.log(this.sectionsList);
     },
