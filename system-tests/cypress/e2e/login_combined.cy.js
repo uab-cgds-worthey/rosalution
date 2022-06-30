9c9b3ad57824e324feba85bee74316785c1d5140
Alexander Moss <amoss01@uab.edu>
2022-06-30 15:35:50 -0500

    created login system test

---
+++ system-tests/cypress/e2e/login_combined.cy.js
@@ -0,0 +1,28 @@
+describe('login_combined.cy.js', () => {
+  it('renders the webpage', () => {
+    cy.visit('local.rosalution.cgds/rosalution')
+    
+  });
+  
+  it('app url redirects to login page', () => {
+    cy.visit('local.rosalution.cgds/rosalution/login')
+    cy.url().should('include', '/login')
+  });
+  
+  it('logs in a user', () => {
+    cy.visit('local.rosalution.cgds/rosalution/login')
+    cy.get('[placeholder="username"]').type('user01')
+    cy.get('[placeholder="password"]').type('secret')
+    cy.get('.center > :nth-child(8)').click()
+    cy.wait(500)
+    cy.get('.center > :nth-child(11)').click()
+    cy.wait(500)
+    cy.get('h1').should('contain', 'Welcome, user01')
+  });
+
+  it('logs out', () => {
+    cy.get('.center > :nth-child(14)').click()
+    cy.wait(500)
+    cy.get('h1').should('contain', 'Logged out successfully!')
+  });
+})
