8b8a310589d892688b0e448178fd4d5cf318c6f1
Alexander Moss <amoss01@uab.edu>
2022-07-07 13:35:53 -0500

    removing unneeded packages & changing cypress.config.js to reflect the same

--- system-tests/cypress.config.js
+++ system-tests/cypress.config.js
@@ -1,26 +1,10 @@
-const { defineConfig } = require('cypress')
-
-// need to install these dev  dependencies
-const _ = require('lodash')
-const del = require('del')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  // setupNodeEvents can be defined in either
-  // the e2e or component configuration
   e2e: {
+    video: false,
     setupNodeEvents(on, config) {
-      on('after:spec', (spec, results) => {
-        if (results && results.video) {
-          // Do we have failures for any retry attempts?
-          const failures = _.some(results.tests, (test) => {
-            return _.some(test.attempts, { state: 'failed' })
-          })
-          if (!failures) {
-            // delete the video if the spec passed and no tests retried
-            return del(results.video)
-          }
-        }
-      })
-    }
-  }
-})
+      // implement node event listeners here
+    },
+  },
+});
