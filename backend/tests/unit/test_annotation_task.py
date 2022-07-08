54282fd0fba78a4caafdeb241721c646cb9cd41c
James Scherer <jscherer@uab.edu>
2022-07-08 14:27:18 -0500

    Updated with new test as well as linting

--- backend/tests/unit/test_annotation_task.py
+++ backend/tests/unit/test_annotation_task.py
@@ -1,7 +1,7 @@
 """Tests Annotation Tasks and the creation of them"""
 import pytest
 from src.enums import GenomicUnitType
-from src.annotation_task import AnnotationTaskFactory, HttpAnnotationTask
+from src.annotation_task import AnnotationTaskFactory, HttpAnnotationTask, recurse
 
 def test_http_annotation_base_url(http_annotation_transcript_id):
     """Verifies if the HTTP annotation creates the base url using the url and genomic_unit as expected."""
@@ -57,6 +57,23 @@ def test_extract_annotations_from_response(http_annotation_task_many_datasets, t
 
     assert 'SIFT Prediction' in actual[second_transcript]
 
+def test_annotation_extraction_recurse(annotation_extraction_recurse_fixtures):
+    """ Tests the annotation task recursive helper function for extracting specific pieces of data from a response """
+    data, attrs, dataset, annotations = annotation_extraction_recurse_fixtures
+
+    actual = recurse(data, attrs, dataset, annotations)
+
+    actual_keys_list = list(actual.keys())
+
+    first_transcript = actual_keys_list[0]
+    second_transcript = actual_keys_list[1]
+
+    assert first_transcript == 'NM_001017980.4'
+    assert second_transcript == 'NM_001363810.1'
+
+    assert 'SIFT Score' in actual[first_transcript]
+    assert actual[second_transcript]['SIFT Score']['value'] == 0.01
+
 @pytest.fixture(name="hgvs_variant_genomic_unit")
 def fixture_genomic_unit():
     """Returns the genomic unit 'NM_170707.3:c.745C>T' to be annotated"""
@@ -104,3 +121,35 @@ def fixture_http_annotation_many_datasets(http_annotation_task_empty, transcript
     for dataset_json in transcript_datasets_json:
         http_annotation_task_empty.append(dataset_json)
     return http_annotation_task_empty
+
+@pytest.fixture(name="annotation_extraction_recurse_fixtures")
+def fixture_annotation_extraction_recurse():
+    """ The components necessary for the recursive function to extract the data """
+    http_response_data = {
+            "transcript_consequences": [
+                {
+                    "transcript_id":"NM_001017980.4",
+                    "sift_score":0.02,
+                },
+                {
+                    "sift_score":0.01,
+                    "transcript_id":"NM_001363810.1",
+                }
+            ],
+        }
+
+    desired_attribute = ["transcript_consequences[]", "sift_score"]
+
+    transcript_dataset = {
+        "data_set": "SIFT Score",
+        "data_source": "Ensembl",
+        "genomic_unit_type": "hgvs_variant",
+        "annotation_source_type": "http",
+        "url": "http://grch37.rest.ensembl.org/vep/human/hgvs/{hgvs_variant}?content-type=application/json",
+        "query_param": "",
+        "attribute": "transcript_consequences[].sift_score"
+    }
+
+    annotations = {}
+
+    return http_response_data, desired_attribute, transcript_dataset, annotations
