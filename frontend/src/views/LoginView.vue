ba80994f013a6aa77641e9b4c636a9e7d59252f2
James Scherer <jscherer@uab.edu>
2022-09-02 16:18:45 -0500

    The oAuth2 login function hard codes 'secret' as a password cause it's just easier, it is just really easier this way

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,33 +1,16 @@
 <template>
-    <div class="center">
-        <h1>{{ this.message }}</h1>
-        <h2 v-if="error != ''" style="color: red">unauthorized</h2>
-          <input v-model="username" placeholder="username"/>
-          <br />
-          <br />
-          <input v-model="password" placeholder="password" type="password"/>
-          <br />
-          <br />
-          <button @click="loginOAuth" type="submit">Login</button>
-          <br />
-          <br />
-          <button @click="verifyUser" type="submit">Verify User</button>
-          <br />
-          <br />
-          <button @click="logoutOAuth" type="submit">Logout</button>
-          <br />
-          <br />
-          <br />
-          <button @click="loginCAS" type="submit">Login BlazerID</button>
-          <br />
-          <br />
-          <button @click="logoutCAS" type="submit">Logout BlazerID</button>
-          <br />
-          <br />
-          <router-link :to="{ path: '/rosalution' }">
-            <button @click="back" type="submit">Back</button>
-          </router-link>
-    </div>
+    <app-content>
+      <div class="center">
+        <img src="@/assets/rosalution-logo.svg" class="rosalution-logo-large" img>
+        <span style="font-weight: bold; font-size: 30px; margin-top: 10px;">Rosalution</span>
+        <span class= "" style="font-weight: 600; font-size: 15px; margin-top: 10px;">Local Development Login</span>
+        <input class="username-input" v-model="username" placeholder="username"/>
+        <button class="login-local-button" @click="loginOAuth" type="submit">Login</button>
+        <!-- <hr width="75%" color="#C4C4C4" size="1"> -->
+        <hr>
+        <button class="login-uab-button" @click="loginCAS" type="submit">UAB LOGIN</button>
+      </div>
+    </app-content>
 </template>
 
 <script>
@@ -62,7 +45,7 @@ export default {
     /* OAuth2 Login Functions */
 
     async loginOAuth() {
-      const userData = {'username': this.username, 'password': this.password};
+      const userData = {'username': this.username, 'password': 'secret'};
 
       const responseJson = await Auth.login(userData);
       console.log(responseJson);
@@ -99,10 +82,74 @@ export default {
 
 <style>
 .center {
-  margin: auto;
-  text-align: center;
-  width: 50%;
-  border: 3px solid green;
-  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 60%;
 }
+
+.rosalution-logo-large {
+  width: 132px;
+  height: 105px;
+  margin-top: 15px;
+}
+
+.username-input {
+  text-align: center;
+  font-size: 13px;
+  margin-top: 25px;
+  border: solid;
+  border-radius: 7px;
+  border-color: var(--rosalution-grey-100);
+  width: 150px;
+  height: 27px;
+}
+
+.username-input::placeholder {
+  color: var(--rosalution-grey-300);
+}
+
+.login-local-button {
+  background-color: var(--rosalution-purple-100);
+  color: var(--rosalution-purple-300);
+  font-family: "Proxima Nova", sans-serif;
+  font-weight: 700;
+  font-size: 18px;
+  height: 1.75rem;
+  width: 7rem;
+  border: none;
+  border-radius: 25px;
+  margin-top: 20px;
+}
+
+.login-local-button:hover {
+  background-color: var(--rosalution-purple-200);
+}
+
+hr {
+  margin-top: 20px;
+  border: none;
+  width: 380px;
+  height: 1px;
+  background-color: var(--rosalution-grey-200);
+}
+
+.login-uab-button {
+  background-color: var(--rosalution-purple-100);
+  color: var(--rosalution-purple-300);
+  font-family: "Proxima Nova", sans-serif;
+  font-weight: 700;
+  font-size: 18px;
+  height: 2.25rem;
+  width: 12rem;
+  border: none;
+  border-radius: 25px;
+  margin-top: 10px; 
+}
+
+.login-uab-button:hover {
+  background-color: var(--rosalution-purple-200);
+}
+
 </style>
