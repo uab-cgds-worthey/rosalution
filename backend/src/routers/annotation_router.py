81749422495996e2b5be33e5099869002e8394d6
Rabab Fatima <rfatima@peds.uab.edu>
2022-09-22 10:26:00 -0500

    Added endpoints for getting annotations by gene and by hgvs variant.

--- backend/src/routers/annotation_router.py
+++ backend/src/routers/annotation_router.py
@@ -1,6 +1,7 @@
 """ Annotation endpoint routes that handle all things annotation within the application """
 
 from fastapi import APIRouter, Depends, BackgroundTasks, HTTPException, status
+from ..enums import GenomicUnitType
 
 from ..core.annotation import AnnotationService
 from ..dependencies import database, annotation_queue
@@ -34,3 +35,42 @@ def annotate_analysis(
     background_tasks.add_task(AnnotationService.process_tasks, annotation_task_queue, collections['genomic_unit'])
 
     return {"name": f"{name} annotations queued."}
+
+@router.get("/annotate-beat")
+def heartbeat():
+    """Returns a heart-beat that orchestration services can use to determine if the application is running"""
+    return "thump-thump"
+
+@router.get("/gene/{gene}")
+def get_annotations_by_gene(gene, rosalution_db=Depends(database)):
+    """Returns annotations data by calling method to find annotations by gene"""
+
+    genomic_unit = {
+        'type': GenomicUnitType.GENE,
+        'unit': gene,
+    }
+
+    queried_genomic_unit = rosalution_db["genomic_unit"].find_genomic_unit(genomic_unit)
+
+    if queried_genomic_unit is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+
+    return queried_genomic_unit['annotations']
+
+@router.get("/hgvsVariant/{variant}")
+def get_annotations_by_hgvs_variant(variant: str, rosalution_db=Depends(database)):
+    """Returns annotations data by calling method to find annotations for variant and relevant transcripts
+    by HGVS Variant"""
+
+    genomic_unit = {
+        'type': GenomicUnitType.HGVS_VARIANT,
+        'unit': variant,
+    }
+
+    queried_genomic_unit = rosalution_db["genomic_unit"].find_genomic_unit(genomic_unit)
+
+    if queried_genomic_unit is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+
+
+    return queried_genomic_unit['annotations'] + queried_genomic_unit['transcripts']
