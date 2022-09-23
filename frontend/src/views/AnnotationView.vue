1c23a9cdfa7bf5de4d33ad19123ea72e450c4153
SeriousHorncat <ange.unoantonison@gmail.com>
2022-09-23 13:06:53 -0500

    Work in progress to figure out how to render section headers programmatically from a render configuration.

--- frontend/src/views/AnnotationView.vue
+++ frontend/src/views/AnnotationView.vue
@@ -1,20 +1,54 @@
 <template>
-  <div data-test="annotations">
-    This is the Annotation View
-  </div>
+  <app-header>
+    <AnnotationViewHeader :titleText="this.analysis_name" :genes="[this.gene]" :variants="[this.variant]">
+    </AnnotationViewHeader>
+  </app-header>
+  <app-content>
+    <AnnotationSection
+      v-for="(section, index) in this.rendering" :key="`${section.type}-${index}`"
+      :header="this[section.header] ? this[section.header] : section.header"
+    >
+    </AnnotationSection>
+  </app-content>
 </template>
 
 <script>
+import Analyses from '@/models/analyses.js';
+import Auth from '@/models/authentication.js';
+
+import AnnotationSection from '@/components/AnnotationView/AnnotationSection.vue';
+import AnnotationViewHeader from '@/components/AnnotationView/AnnotationViewHeader.vue';
+
 export default {
   name: 'annotation-view',
+  components: {
+    AnnotationSection,
+    AnnotationViewHeader,
+  },
+  props: ['analysis_name', 'gene', 'variant'],
+  data: function() {
+    return {
+      rendering: [],
+      username: '',
+    };
+  },
+  created() {
+    this.getUsername();
+    this.getRenderingConfiguration();
+  },
+  methods: {
+    async getUsername() {
+      const fetchUser = await Auth.getUser();
+      console.log(fetchUser);
+      this.username = fetchUser['username'];
+    },
+    async getRenderingConfiguration() {
+      this.rendering.push(...await Analyses.getAnnotationConfiguration(this.analysis_name));
+    },
+  },
 };
 </script>
 
 <style scoped>
-
-div {
-  font-family: "Proxima Nova", sans-serif;
-  padding: var(--p-0);
-}
 
 </style>
