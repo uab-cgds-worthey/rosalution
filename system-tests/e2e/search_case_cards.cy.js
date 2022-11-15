900ea4098731b00c64e900bcf124f9833f9a90fa
Alexander Moss <amoss01@uab.edu>
2022-11-15 11:00:36 -0600

    Create search_case_cards.cy.js

---
+++ system-tests/e2e/search_case_cards.cy.js
@@ -0,0 +1,17 @@
+describe('search_case_cards.cy.js', () => {
+
+  it('searches for cases', () => {
+    cy.visit('/login');
+    cy.get('[data-test="username-input"]').type('user01');
+    cy.get('[data-test="local-login-button"]').click();
+    cy.get('[data-test="analysis-search"]').type('c.164G>T');
+    cy.get('.analysis-card > .analysis-base').should('contain', 'c.164G>T');
+    cy.get('[data-test="analysis-search"]').clear();
+    cy.get('[data-test="analysis-search"]').type('CMT4B3 Foundation');
+    cy.get('.analysis-card > .analysis-base').should('contain', 'CMT4B3 Foundation');
+    cy.get('[data-test="analysis-search"]').clear();
+    cy.get('[data-test="analysis-search"]').type('CPAM0053');
+    cy.get('.analysis-card > .analysis-base').should('contain', 'CPAM0053');
+    cy.get('[data-test="analysis-search"]').clear();
+  });
+});
