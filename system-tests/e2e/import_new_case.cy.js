de7757b9a1fbe614361a9b216db449e7c85babf1
Alexander Moss <amoss01@uab.edu>
2022-11-18 15:51:22 -0600

    Create import_new_case.cy.js

---
+++ system-tests/e2e/import_new_case.cy.js
@@ -0,0 +1,65 @@
+describe('import_new_case.cy.js', () => {
+  beforeEach(() => {
+    cy.resetDatabase();
+  });
+
+  it('imports a new case', () => {
+    cy.visit('/login');
+    cy.get('[data-test="username-input"]').type('user01');
+    cy.get('[data-test="local-login-button"]').click();
+    cy.get('.analysis-create-card').click();
+    cy.get('.drop-file-box-content').selectFile('../backend/tests/fixtures/phenotips-import.json', {
+      action: 'drag-drop',
+    });
+    cy.get('[data-test="confirm"]').click();
+    cy.get('h2').should('contain', 'Successful import');
+    cy.get('.modal-container').should('exist');
+    cy.get('[data-test="confirm-button"]').click();
+    cy.get('.modal-container').should('not.exist');
+    cy.visit('/');
+    cy.get('app-content').should('contain', 'CPAM0112');
+  });
+
+  it('imports a new case with a duplicate case ID', () => {
+    cy.visit('/login');
+    cy.get('[data-test="username-input"]').type('user01');
+    cy.get('[data-test="local-login-button"]').click();
+    cy.get('.analysis-create-card').click();
+    cy.get('.drop-file-box-content').selectFile('../backend/tests/fixtures/phenotips-import.json', {
+      action: 'drag-drop',
+    });
+    cy.get('[data-test="confirm"]').click();
+    cy.get('h2').should('contain', 'Successful import');
+    cy.get('.modal-container').should('exist');
+    cy.get('[data-test="confirm-button"]').click();
+    cy.get('.modal-container').should('not.exist');
+    cy.visit('/');
+    cy.get('app-content').should('contain', 'CPAM0112');
+    cy.get('.analysis-create-card').click();
+    cy.get('.drop-file-box-content').selectFile('../backend/tests/fixtures/phenotips-import.json', {
+      action: 'drag-drop',
+    });
+    cy.get('[data-test="confirm"]').click();
+    cy.get('h2').should('contain', 'Failed to import phenotips analysis');
+    cy.get('.modal-container > span').should('contain', 'Error: Status Code: 409 Conflict');
+    cy.get('.modal-container').should('exist');
+    cy.get('[data-test="confirm-button"]').click();
+    cy.get('.modal-container').should('not.exist');
+  });
+
+  it('tries to import a new case with a file that isn\'t a JSON file', () => {
+    cy.visit('/login');
+    cy.get('[data-test="username-input"]').type('user01');
+    cy.get('[data-test="local-login-button"]').click();
+    cy.get('.analysis-create-card').click();
+    cy.get('.drop-file-box-content').selectFile('../backend/tests/fixtures/example_file_to_upload.txt', {
+      action: 'drag-drop',
+    });
+    cy.get('[data-test="confirm"]').click();
+    cy.get('h2').should('contain', 'Failed to import phenotips analysis');
+    cy.get('.modal-container > span').should('contain', 'Error: Status Code: 500 Internal Server Error');
+    cy.get('.modal-container').should('exist');
+    cy.get('[data-test="confirm-button"]').click();
+    cy.get('.modal-container').should('not.exist');
+  });
+});
