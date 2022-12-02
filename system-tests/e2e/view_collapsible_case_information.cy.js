b5888f159054a71b9d768e8b33a87c78ab5bd5d5
Alexander C. Moss <amoss01@uab.edu>
2022-12-02 11:34:40 -0600

    Create view_collapsible_case_information.cy.js

---
+++ system-tests/e2e/view_collapsible_case_information.cy.js
@@ -0,0 +1,24 @@
+describe('view_collapsible_case_information.cy.js', () => {
+  beforeEach(() => {
+    cy.resetDatabase();
+    cy.visit('/login');
+    cy.get('[data-test="username-input"]').type('user01');
+    cy.get('[data-test="local-login-button"]').click();
+    cy.get('.analysis-card').first().click();
+  });
+
+  it('can view the case information when expanded and not when its collapsed', () => {
+    cy.get('#Brief > tbody > :nth-child(4) > :nth-child(1) > .field').should('be.visible');
+    cy.get('#Brief > tbody > :nth-child(5) > .values > [data-test="value-row"]').should('be.visible');
+    cy.get('#Clinical_History > tbody > :nth-child(4) > :nth-child(1) > .field').should('be.visible');
+    cy.get(':nth-child(10) > .values > :nth-child(3)').should('be.visible');
+    cy.get('#Brief > tbody > .section-header > .section-header-content > .collapsable-logo > .svg-inline--fa').click();
+    cy.get(
+        '#Clinical_History > tbody > .section-header > .section-header-content > .collapsable-logo > .svg-inline--fa')
+        .click();
+    cy.get('#Brief > tbody > :nth-child(4) > :nth-child(1) > .field').should('not.be.visible');
+    cy.get('#Brief > tbody > :nth-child(5) > .values > [data-test="value-row"]').should('not.be.visible');
+    cy.get('#Clinical_History > tbody > :nth-child(4) > :nth-child(1) > .field').should('not.be.visible');
+    cy.get(':nth-child(10) > .values > :nth-child(3)').should('not.be.visible');
+  });
+});
