3b51891eb5d5128eb59a394c2bbd632a5aa6d40b
Alexander Moss <amoss01@uab.edu>
2022-12-05 16:59:04 -0600

    Create case_annotation_display_transcripts.cy.js

---
+++ system-tests/e2e/case_annotation_display_transcripts.cy.js
@@ -0,0 +1,25 @@
+describe('case_annotation_display_transcripts.cy.js', () => {
+  beforeEach(() => {
+    cy.resetDatabase();
+    cy.visit('/login');
+    cy.get('[data-test="username-input"]').type('user01');
+    cy.get('[data-test="local-login-button"]').click();
+    cy.get('.analysis-card').first().click();
+  });
+
+  it('Displays the transcript in the gene', () => {
+    cy.get('[data-test="gene-name"]').click();
+    cy.get(':nth-child(1) > .transcript-header > .transcript-header-text').should('exist');
+    cy.get(':nth-child(1) > .transcript-header > .transcript-header-text').should('contain', 'NM_001017980.4');
+    cy.get(':nth-child(2) > .transcript-header > .transcript-header-text').should('exist');
+    cy.get(':nth-child(2) > .transcript-header > .transcript-header-text').should('contain', 'NM_001363810.1');
+  });
+
+  it('Displays the transcript in the variant', () => {
+    cy.get('[data-test="c-dot"]').click();
+    cy.get(':nth-child(1) > .transcript-header > .transcript-header-text').should('exist');
+    cy.get(':nth-child(1) > .transcript-header > .transcript-header-text').should('contain', 'NM_001017980.4');
+    cy.get(':nth-child(2) > .transcript-header > .transcript-header-text').should('exist');
+    cy.get(':nth-child(2) > .transcript-header > .transcript-header-text').should('contain', 'NM_001363810.1');
+  });
+});
