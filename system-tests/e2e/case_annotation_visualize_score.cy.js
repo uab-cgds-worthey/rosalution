2f0e82e5ed2390619e2a7030eaeb2b8c300cf5ef
Alexander Moss <amoss01@uab.edu>
2022-12-09 17:22:51 -0600

    Create case_annotation_visualize_score.cy.js

---
+++ system-tests/e2e/case_annotation_visualize_score.cy.js
@@ -0,0 +1,27 @@
+describe('case_annotation_display_transcripts.cy.js', () => {
+  beforeEach(() => {
+    cy.visit('/login');
+    cy.get('[data-test="username-input"]').type('user01');
+    cy.get('[data-test="local-login-button"]').click();
+  });
+
+  it('Displays Blue to represent Nominal, and Yellow to represent potentially relevant', () => {
+    cy.get('.analysis-card').first().click();
+    cy.get('[data-test="gene-name"]').click();
+    cy.get('[data-test="score-background"]').should('exist');
+    cy.get('[data-test="score-background"]').should('have.attr', 'style', 'background-color:' +
+    ' var(--rosalution-blue-300); border-color: 2px solid var(--rosalution-blue-100);');
+
+    cy.get(':nth-child(1) > .transcript-header > .dataset-container > .highlight-text').should('exist');
+    cy.get(':nth-child(1) > .transcript-header > .dataset-container > .highlight-text').should('have.attr', 'style',
+        'background-color: var(--rosalution-yellow-200);');
+  });
+
+  it('displays Red to represent Relevant', () => {
+    cy.get('[href="/rosalution/analysis/CPAM0047"]').click();
+    cy.get('[data-test="gene-name"]').click();
+    cy.get(':nth-child(1) > .transcript-header > .dataset-container > .highlight-text').should('exist');
+    cy.get(':nth-child(1) > .transcript-header > .dataset-container > .highlight-text').should('have.attr', 'style',
+        'background-color: var(--rosalution-red-200);');
+  });
+});
