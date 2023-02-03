89f1dc4013b5aba5043224cab3210b7d72466a4a
Angelina Uno-Antonison <ange.unoantonison@gmail.com>
2023-02-03 10:08:54 -0600

    Added an update to the entrpoint that supports it expanding all of the positional arguments from the entrypoint script to the uvicorn application for the backend service. (#37)

--- backend/etc/entrypoint-init.sh
+++ backend/etc/entrypoint-init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ROSALUTION_KEY="$(< /dev/urandom tr -dc A-Za-z0-9  | head -c 65)" && export ROSALUTION_KEY && \
-   uvicorn src.main:app --host 0.0.0.0 --port 8000 --log-level info
+   uvicorn src.main:app --host 0.0.0.0 --port 8000 --log-level info "$@"
