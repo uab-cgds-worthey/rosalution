c7d449a44515324937fc9f90b3b0935a206ec08b
James Scherer <jscherer@uab.edu>
2023-02-08 09:56:59 -0600

    Fixed some rebasing issues

--- backend/src/routers/auth_router.py
+++ backend/src/routers/auth_router.py
@@ -198,16 +198,19 @@ def generate_secret(client_id: VerifyUser = Security(get_current_user), reposito
 
     return credentialed_user
 
+
 @router.get('/get_user_credentials')
 def fetch_user_api_creds(client_id: VerifyUser = Security(get_current_user), repositories=Depends(database)):
+    """ Returns a special pydantic object including the user's client id and client secret """
     user = repositories['user'].find_by_client_id(client_id)
-    
+
     if not user:
         raise HTTPException(status_code=500, detail="Something went wrong. Unable to create client secret.")
 
-    credentialed_user = UserAPI(**user)
+    credentialed_user = AccessUserAPI(**user)
 
     return credentialed_user
+
 
 @router.get("/logout")
 def logout_oauth(request: Request, response: Response, settings: Settings = Depends(get_settings)):
