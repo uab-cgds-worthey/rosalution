375aaa253724c57bf2e6e7fbf5363fdaf72c6d63
James Scherer <jscherer@uab.edu>
2023-02-22 15:05:12 -0600

    Updated the script's documentation and added an alternate CPAM0074 to test multiple deletions

--- etc/database/remove_analysis.sh
+++ etc/database/remove_analysis.sh
@@ -1,47 +1,56 @@
 #! /bin/sh
-# ./initial-db-seed.sh -h <host> -f <datasource>
+# ./remove_analysis.sh -c <mongo_connection_string> -d <docker_container_name> -m <mongo_database>
 usage() {
-  echo "usage: $0 -h <host>"
   echo " "
-  echo " -h MongoDB host URL"
+  echo "usage: $0 -c <mongo_connection_string> -d <docker_container_name> -m <mongo_database>"
+  echo " "
+  echo " -c MongoDB connection string"
   echo "    (default) localhost:27017"
+  echo " -d Docker container running Mongo"
+  echo "    Find with 'docker ps' command"
+  echo " -m Mongo database name"
+  echo "    (default) rosalution_db"
+  echo " -h Usage"
   echo " "
-  echo "Seeds the initial Rosalution database with base fixtures for local development and system testing"
+  echo "Removes Rosalution Analyses from Mongo."
   exit
 }
 
-analysisName=""
 docker_prefix=""
 connection_string="localhost:27017"
+database="rosalution_db"
 
-while getopts ":a:d:h" opt; do
+while getopts ":c:d:m:h" opt; do
   case $opt in
-    a) mongodb_host=$OPTARG;;
-    d) docker_prefix="docker exec -t ${OPTARG}";;
     c) connection_string=$OPTARG;;
+    d) docker_prefix="docker exec ${OPTARG}";;
+    m) database=$OPTARG;;
     h) usage;;
     \?) echo "Invalid option -$OPTARG" && exit 127;;
   esac
 done
 
-database="rosalution_db"
-
-in='$in'
-
 echo "Which Analysis would you like to remove:"
 read analysisName
 
 echo "Finding Analysis..."
-analysis=$(${docker_prefix} mongosh --quiet --eval "'db.analyses.find({'name': {$in: ['$analysisName']}}, {_id: true});'" $database | cut -d'"' -f 2)
+analysis=$(${docker_prefix} mongosh --quiet --eval "'db.analyses.find({'name': /$analysisName/}).count();'" $database)
 
-echo $analysis
-
-if [ -z "$analysis" ]; then
+if [ "$analysis" = "0" ]; then
    echo "${analysisName} not found.";
    exit;
 fi
 
-echo "Found ${analysis} of ${analysisName}. Would you like to remove them [Y/n]:"
-# read response
+read -p "Found ${analysis} of ${analysisName}. Would you like to remove them [y/N]: " -n 1 -r
+echo
 
-# echo "Removing "
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+  echo "Cancelling."
+  exit;
+fi
+
+echo "Removing..."
+${docker_prefix} mongosh --quiet --eval "'db.analyses.deleteMany({'name': /$analysisName/});'" $database
+
+echo "Done."
