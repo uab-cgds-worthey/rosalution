5b400b8082e5d09e589c43a429ad0d0982672e8a
James Scherer <jscherer@uab.edu>
2023-04-21 12:19:04 -0500

    Finally rebased and updated the tests to work with new ticks

--- frontend/src/fileRequests.js
+++ frontend/src/fileRequests.js
@@ -3,7 +3,6 @@ import Requests from '@/requests.js';
 export default {
   async getImage(fileId) {
     const url = `/rosalution/api/analysis/download/${fileId}`;
-    console.log(url);
     return await Requests.getImage(url);
   },
 };
