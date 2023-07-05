17db0c52d62647627f3a5343e70c17c9ec7c7c55
Rabab Fatima <rfatima@uab.edu>
2023-07-05 17:13:17 -0500

    Added migration script to reconfigure analysis sections. Gene sections throwing error now

--- etc/fixtures/migrations/migrate-new-sections-phenotips-importer.js
+++ etc/fixtures/migrations/migrate-new-sections-phenotips-importer.js
@@ -7,7 +7,7 @@ mongosh /tmp/fixtures/migrations/migrate-new sections-phenotips-importer.js
     
     Example: 
     
-    mongosh --host localhost --port 27017 --eval "var help=true;" /tmp/fixtures/migrations/migrate-new-sections-phenotips-importer.js
+    mongosh --host localhost --port 27017 --eval 'var help=true;' /tmp/fixtures/migrations/migrate-new-sections-phenotips-importer.js
     
     docker exec -it <rosalution_db_container> mongosh /tmp/fixtures/migrations/migrate-new-sections-phenotips-importer.js
 `
@@ -18,9 +18,9 @@ if (help === true) {
 }
 
 if (typeof databaseName === 'undefined') {
-    databaseName = "rosalution_db";
+    databaseName = 'rosalution_db';
 } else if (typeof databaseName !== 'string') {
-    print("databaseName must be a string");
+    print('databaseName must be a string');
     quit(1);
 }
 
@@ -28,20 +28,20 @@ if (typeof databaseName === 'undefined') {
 // can we just use the config in phenotips importer? without creating an array/object here?
 // const newSectionsRename = [
 //   {
-//       "header": str(phenotips_json_data["gene"] + "Gene to Phenotype (like pedigree)"), "content":[]
+//       'header': str(phenotips_json_data['gene'] + 'Gene to Phenotype (like pedigree)'), 'content':[]
 //   }, {
-//       "header": str(phenotips_json_data["gene"] + "Molecular Mechanism"), "content":[
-//           {"field": 'Function', "value": []}
+//       'header': str(phenotips_json_data['gene'] + 'Molecular Mechanism'), 'content':[
+//           {'field': 'Function', 'value': []}
 //       ]
 //   },{
-//       "header": str(phenotips_json_data["gene"] + "Function"), "content":[]
+//       'header': str(phenotips_json_data['gene'] + 'Function'), 'content':[]
 //   },{
-//       "header": 'Model Goals', "content": [
-//           {"field": 'Model of Interest', "value": []},
-//           {"field": 'Goals', "value": []},
-//           {"field": 'Proposed Model/Project', "value": []},
-//           {"field": 'Existing Collaborations', "value": []},
-//           {"field": 'Existing Funding', "value": []},
+//       'header': 'Model Goals', 'content': [
+//           {'field': 'Model of Interest', 'value': []},
+//           {'field': 'Goals', 'value': []},
+//           {'field': 'Proposed Model/Project', 'value': []},
+//           {'field': 'Existing Collaborations', 'value': []},
+//           {'field': 'Existing Funding', 'value': []},
 //     ]
 //   }
 // ]
@@ -61,24 +61,88 @@ try {
         genes.push(unit.gene)
       }
     })
-    print(genes)
 
-    //iterating through the genes array to append the gene sections here
-    genes.forEach(gene => {
-      print ("APPENDING GENE SECTIONS HERE TO element.sections")
-    })
+    //iterating through the genes array to create the gene sections here
+    if (genes.length > 0) {
+      genes.forEach(gene => {
+        print (`${gene} APPENDING GENE SECTIONS HERE TO element.sections`)
+        newGeneSections = [{
+            "header": gene.concat(" Gene to Phenotype"), "content": [
+                {"type": "images-dataset", "field": 'Gene to Phenotype', "value": []},
+                {
+                    "type": "section-text", "field": 'HPO Terms',
+                    // move HPO TERMS HERE HOW???
+                    // Need to get it from Clinical History
+                    "value": "HPO TERMS TO BE MOVED HERE FROM CLINICAL HISTORY"
+                },
+            ]
+        }, {
+            "header": gene.concat(" Molecular Mechanism"),
+            "content": [{"type": "section-text", "field": 'Function Overview', "value": []}]
+        }, {
+            "header": gene.concat(" Function"),
+            "content": [{"type": "images-dataset", "field": 'Function', "value": []},]
+        }]
+      })
 
+      element.sections.push(newGeneSections)
+    }
+    
+
+    let briefToModelGoalsFields = [];
+    let namesOfFieldsToMove = ['Model of Interest', 'Goals', 'Proposed Model/Project']
     element.sections.forEach(section => {
+      // print(section)
       const value = Object.values(section)
       
       // print(value)
 
+      if(section.header === 'Brief') {
+        section.content.forEach(contentItem => {
+          contentItem['type'] = 'section-text';
+          if(namesOfFieldsToMove.includes(contentItem.field)) {
+            briefToModelGoalsFields.push(contentItem);
+          }
+        });
+
+        section.content = section.content.filter((contentItem) => {
+          return !namesOfFieldsToMove.includes(contentItem.field);
+        });
+      } else if (section.header === 'Pedigree') {
+        section['attachment_field'] = 'Pedigree';
+        if(section.content.length == 0) {
+          section.content.push[{
+            'type': 'images-dataset',
+            'field': 'Pedigree',
+            'value': []
+          }]
+        } else {
+          print('pedigree section values');
+          // take the existing value and put it in the new object format and
+          // replace it in the field
+        }
+      } else {
+        section.content.forEach(contentItem => { 
+          contentItem['type'] = 'section-text';
+        });
+      }
       
       // add new sections to sections[] in each element
       // check if field already exists in analyses sections 
       //move field data -- how????
 
     })
+
+    element.sections.push({
+      header: 'Model Goals', 'content': [
+       ...briefToModelGoalsFields,
+        {'field': 'Existing Collaborations', 'value': [], type: 'section-text'},
+        {'field': 'Existing Funding', 'value': [], type: 'section-text'},
+      ]
+    })
+
+    // print(briefToModelGoalsFields)
+    print(element.sections);
 
     // update       
     // db.analyses.update(
