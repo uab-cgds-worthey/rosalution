bd0bd18be0bc4f1f1ad9cf2be97343283c1bbeac
Angelina Uno-Antonison <ange.unoantonison@gmail.com>
2023-08-16 13:37:33 -0500

    Fixed the logout menu to work on the annotation page; the header at top on analysis page is multuple rows and the icons don't awkwardly sit on top of each other; created a menu-button css style to store the styling of the menu anchors to be consistent across the application;

--- frontend/src/components/AnnotationView/AnnotationSidebar.vue
+++ frontend/src/components/AnnotationView/AnnotationSidebar.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <a v-for="(anchor,index) in this.sectionAnchors" :href="`#${anchor}`" :key="`${anchor}-${index}`">
+    <a v-for="(anchor,index) in this.sectionAnchors"
+      :href="`#${anchor}`"
+      :key="`${anchor}-${index}`"
+      class="menu-button"
+    >
       {{ toAnchorText(anchor) }}
     </a>
   </div>
@@ -21,35 +25,15 @@ export default {
 </script>
 
 <style scoped>
-div {
-  display: inline-flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  background-color: var(--rosalution-white);
-  border-radius: var(--content-border-radius);
-  padding: var(--p-8);
-  margin-top: var(--p-10);
-  margin-left: var(--p-10);
-  margin-bottom: var(--p-10);
-  overflow-x: hidden;
-  max-height: 100vh;
-  width: fit-content;
-}
-
-div a {
-  border-radius: var(--content-border-radius);
-  background-color: var(--primary-background-color);
-  color: var(--rosalution-purple-200);
-  padding: var(--p-5);
-  margin: var(--p-5);
-  font-size: 1.125rem; /* 18px */
-  font-weight: 700;
-  width: fit-content;
-}
-
-/*
-div h2 {
-  margin: 0;
-  flex: 2 1 auto;
-} */
+  div {
+    display: inline-flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    background-color: var(--rosalution-white);
+    border-radius: var(--content-border-radius);
+    padding: var(--p-8);
+    gap: var(--p-8);
+    margin-top: var(--p-10);
+    overflow-x: hidden;
+  }
 </style>
