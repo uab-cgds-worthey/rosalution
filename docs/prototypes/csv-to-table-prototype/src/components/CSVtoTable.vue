d4e3b88056eb160e8df9d2b1f26672b2c29e8b5e
James Scherer <jscherer@uab.edu>
2023-10-13 15:33:41 -0500

    Experiment with new Custom Component as Section Row Item for Animal Model (#143)
    * Updated Rosalution to have the prototype of turning a csv into a table in html
    
    * CSV to Table prototype component

---
+++ docs/prototypes/csv-to-table-prototype/src/components/CSVtoTable.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="rosalution-section-container">
+      <div class="rosalution-section-header">
+      <h2 class="rosalution-section-header-text">
+          Animal Models
+      </h2>
+      </div>
+      <div class="rosalution-section-seperator"></div>
+      <div class="section-content">
+          <br>
+          <textarea style="width: 400px; height: 100px;" v-model="modelText"/>
+          <p  v-html="convertToTable"></p>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'csv-to-table',
+  data() {
+      return {
+          section_toggle: 'animal models' + '_collapse',
+          modelText: ''
+      }
+  },
+  computed: {
+      convertToTable() {
+        if (this.modelText == '')
+            return
+        
+        var data = this.modelText
+        var lines = data.split("\n"), output = [], i;
+        
+        for (i = 0; i < lines.length; i++)
+            output.push(
+                "<tr><td>"
+                + lines[i].split(",").join("</td><td>")
+                + "</td></tr>"
+            );
+
+        return `<table>` + output.join("") + `</table>`;
+      }
+    }
+}
+</script>
+
+<!--
+    The styling doesn't appear to work if it's scoped,
+    which means anything with this class will be affected
+    within Rosalution
+-->
+<style>
+
+table, th, td {
+  border: 1px solid;
+}
+
+</style>
---
+++ docs/prototypes/csv-to-table-prototype/src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+
+import './styles/main.css';
+import './styles/rosalution.css';
+
+import App from './App.vue'
+
+createApp(App).mount('#app')
