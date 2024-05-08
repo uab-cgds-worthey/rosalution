3d1b9c2572fe7153b1c31108eb28d05e18fd94af
Rabab Fatima <rfatima@uab.edu>
2024-05-08 15:13:29 -0500

    Unit tests are passing for annotation_unit. Formatted and linted backend.

--- backend/tests/unit/core/test_annotation_unit.py
+++ backend/tests/unit/core/test_annotation_unit.py
@@ -1,4 +1,5 @@
 """Tests for annotation unit class"""
+from unittest.mock import Mock
 import pytest
 
 from src.core.annotation_unit import AnnotationUnit
@@ -9,15 +10,28 @@ def test_annotation_unit_gets_missing_dependencies(annotation_unit_lmna):
     actual = annotation_unit_lmna.get_missing_dependencies()
     assert actual == ['HGNC_ID']
 
+
 def test_annotation_unit_ready_for_annotation(annotation_unit_lmna):
     """Verifies if the annotation unit is ready for annotation"""
-    actual = annotation_unit_lmna.ready_for_annotation()
-    assert actual == False
+
+    missing = annotation_unit_lmna.get_missing_dependencies()
+    missing_dependency = missing[0]
+    mock_genomic_unit_collection = Mock()
+    mock_genomic_unit_collection.find_genomic_unit_annotation_value = Mock()
+    annotation_value = mock_genomic_unit_collection.find_genomic_unit_annotation_value(
+        annotation_unit_lmna.genomic_unit, missing_dependency
+    )
+
+    actual = annotation_unit_lmna.ready_for_annotation(annotation_value, missing_dependency)
+    assert actual is True
+
 
 def test_annotation_unit_should_continue_annotation(annotation_unit_lmna):
     """Verifies if the annotation unit should continue annotation"""
-    actual = annotation_unit_lmna.should_continue_annotation()
-    assert actual == ""
+    actual_missing_dependencies, actual_logger_message = annotation_unit_lmna.should_continue_annotation()
+    assert actual_missing_dependencies == ""
+    assert actual_logger_message == '%s Delaying Annotation, Missing Dependency...'
+
 
 @pytest.fixture(name="annotation_unit_lmna")
 def fixture_annotation_unit():
