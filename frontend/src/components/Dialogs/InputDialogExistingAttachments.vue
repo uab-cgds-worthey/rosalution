c9bc8fc2d6106c49659d52094b6f89f7059120cb
Rabab Fatima <rfatima@uab.edu>
2024-12-17 10:25:45 -0600

    creating 3rd tab for input dialog - to attach existing evidence

---
+++ frontend/src/components/Dialogs/InputDialogExistingAttachments.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="attach-supporting-evidence">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'input-dialog-existing-attachments',
+  emits: ['update:userInput'],
+  props: {
+    userInput: {
+      type: Object,
+      required: true,
+    },
+    existingAttachments: {
+      type: Array,
+      default: () => {
+        return [];
+      },
+    },
+  },
+  computed: {
+  },
+  methods: {
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
--- frontend/src/views/AnalysisView.vue
+++ frontend/src/views/AnalysisView.vue
@@ -597,6 +597,7 @@ async function openDiscussionModal(postId) {
       .cancelText('Cancel')
       .file(includeComments, 'file', '.png, .jpg, .jpeg, .bmp')
       .url(includeComments, includeName)
+      .existing(includeComments, 'rosalution')
       .prompt();
 
   if (!attachment) {
