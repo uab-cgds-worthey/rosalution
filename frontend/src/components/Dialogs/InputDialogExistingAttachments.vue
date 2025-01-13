6f5d114b3c9f5d15f41c540b4bd1e62d56662a66
Rabab Fatima <rfatima@uab.edu>
2025-01-13 11:34:51 -0600

    Added checkmark vector styling for checkbox.

--- frontend/src/components/Dialogs/InputDialogExistingAttachments.vue
+++ frontend/src/components/Dialogs/InputDialogExistingAttachments.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="existing-attachments-list">
-    <div 
+    <div
       class="existing-attachments-list-row"
-      v-for="existingAttachment in existingAttachments" 
+      v-for="existingAttachment in existingAttachments"
       v-bind:key="existingAttachment.attachment_id"
       data-test="existing-attachments-list"
     >
@@ -10,15 +10,17 @@
         <font-awesome-icon :icon="['far', 'file']" size="lg" v-if="existingAttachment.type==='file'"/>
         <font-awesome-icon icon="link" size="lg" v-else-if="existingAttachment.type==='link'"/>
       </span>
-      <label for="existing-attachment-row" class="existing-attachment-name">{{ existingAttachment.name }}</label>
-      <input type="checkbox" class="existing-attachment-checkbox" name="existing-attachment-row"/>
+      <label for="existing-attachment-row" class="existing-attachment-name">
+        {{ existingAttachment.name }}
+        <input type="checkbox" class="existing-attachment-checkbox" name="existing-attachment-row"/>
+      </label>
     </div>
   </div>
 </template>
 
 <script setup>
 console.log('SETUPPING THE THING');
-import {computed, onMounted, reactive} from 'vue';
+import {onMounted} from 'vue';
 
 const emit = defineEmits(['update:userInput']);
 
@@ -76,14 +78,6 @@ onMounted(async () => {
   display:inline-block;
   vertical-align: middle;
 }
-
-/* .existing-attachment-checkbox input {
-  position: absolute;
-  opacity: 0;
-  cursor: pointer;
-  height: 0;
-  width: 0;
-} */
 
 .existing-attachment-checkbox {
   float: right;
