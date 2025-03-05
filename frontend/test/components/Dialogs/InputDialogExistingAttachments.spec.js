a41760ac811aa3da857e7824a5e5d359eac852bb
Rabab Fatima <rfatima@uab.edu>
2025-03-05 10:54:37 -0600

    Update frontend/test/components/Dialogs/InputDialogExistingAttachments.spec.js
    Co-authored-by: James Scherer <jscherer@uab.edu>
    Signed-off-by: Rabab Fatima <fr.rabab@gmail.com>

--- frontend/test/components/Dialogs/InputDialogExistingAttachments.spec.js
+++ frontend/test/components/Dialogs/InputDialogExistingAttachments.spec.js
@@ -37,7 +37,6 @@ describe('InputDialogExistingAttachments.vue', () => {
 
   it('is displaying list of existing evidences', async () => {
     const evidenceList = wrapper.find('[data-test=existing-attachments-list]');
-    console.log(evidenceList.html());
     expect(evidenceList.exists()).to.be.true;
   });
 });
