c2df17626d4ce5860d63e0783317fea8e09302ac
Angelina Uno-Antonison <ange.unoantonison@gmail.com>
2026-07-28 12:31:48 -0500

    Upgraded crypress to latest available version; Upgraded eslint to latest available version;  Upgraded eslint configuration to use defineConfig to match documentation and additional type saftey available in tooling; Upgraded @stylistic/eslint-plugin-js to just @stylistic/eslint-plugin to conform to the combination of the stylistic configurations into a single plugin; updated configuraiton to match;  includes updating deprecated rules to new recommended similar rules; adds names to the linting configuration to navigate which part of a config is being applied to the files; making it easier to explore using 'npx eslint --inspect-config'

--- system-tests/eslint-config-cgds.js
+++ system-tests/eslint-config-cgds.js
@@ -1,8 +1,9 @@
-import stylistic from '@stylistic/eslint-plugin-js';
+import stylistic from '@stylistic/eslint-plugin';
 
 export default [{
+  name: 'cgds',
   plugins: {
-    '@stylistic/js': stylistic,
+    '@stylistic': stylistic,
   },
   rules: {
     // Possible Errors
@@ -27,19 +28,19 @@ export default [{
     'no-unused-vars': ['error', {args: 'none'}],
 
     // Stylistic
-    '@stylistic/js/array-bracket-newline': 'off',
-    '@stylistic/js/array-bracket-spacing': ['error', 'never'],
-    '@stylistic/js/array-element-newline': 'off',
-    '@stylistic/js/block-spacing': ['error', 'never'],
-    '@stylistic/js/brace-style': 'error',
+    '@stylistic/array-bracket-newline': 'off',
+    '@stylistic/array-bracket-spacing': ['error', 'never'],
+    '@stylistic/array-element-newline': 'off',
+    '@stylistic/block-spacing': ['error', 'never'],
+    '@stylistic/brace-style': 'error',
     'camelcase': ['error', {properties: 'never'}],
-    '@stylistic/js/comma-dangle': ['error', 'always-multiline'],
-    '@stylistic/js/comma-spacing': 'error',
-    '@stylistic/js/comma-style': 'error',
-    '@stylistic/js/computed-property-spacing': 'error',
-    '@stylistic/js/eol-last': 'error',
-    '@stylistic/js/func-call-spacing': 'error',
-    '@stylistic/js/indent': [
+    '@stylistic/comma-dangle': ['error', 'always-multiline'],
+    '@stylistic/comma-spacing': 'error',
+    '@stylistic/comma-style': 'error',
+    '@stylistic/computed-property-spacing': 'error',
+    '@stylistic/eol-last': 'error',
+    '@/func-call-spacing': 'error',
+    '@stylistic/indent': [
       'error', 2, {
         'CallExpression': {
           'arguments': 2,
@@ -60,10 +61,10 @@ export default [{
         ],
       },
     ],
-    '@stylistic/js/key-spacing': 'error',
-    '@stylistic/js/keyword-spacing': 'error',
-    '@stylistic/js/linebreak-style': 'error',
-    '@stylistic/js/max-len': ['error', {
+    '@stylistic/key-spacing': 'error',
+    '@stylistic/keyword-spacing': 'error',
+    '@stylistic/linebreak-style': 'error',
+    '@stylistic/max-len': ['error', {
       code: 120,
       tabWidth: 2,
       ignoreUrls: true,
@@ -71,43 +72,45 @@ export default [{
     }],
     'new-cap': 'error',
     'no-array-constructor': 'error',
-    '@stylistic/js/no-mixed-spaces-and-tabs': 'error',
-    '@stylistic/js/no-multiple-empty-lines': ['error', {max: 2}],
-    'no-new-object': 'error',
-    '@stylistic/js/no-tabs': 'error',
-    '@stylistic/js/no-trailing-spaces': 'error',
-    '@stylistic/js/object-curly-spacing': 'error',
+    '@stylistic/no-mixed-spaces-and-tabs': 'error',
+    '@stylistic/no-multiple-empty-lines': ['error', {max: 2}],
+    'no-object-constructor': 'error',
+    '@stylistic/no-tabs': 'error',
+    '@stylistic/no-trailing-spaces': 'error',
+    '@stylistic/object-curly-spacing': 'error',
     'one-var': ['error', {
       var: 'never',
       let: 'never',
       const: 'never',
     }],
-    '@stylistic/js/operator-linebreak': ['error', 'after'],
-    '@stylistic/js/padded-blocks': ['error', 'never'],
-    '@stylistic/js/quote-props': ['error', 'consistent'],
-    '@stylistic/js/quotes': ['error', 'single', {allowTemplateLiterals: true}],
-    '@stylistic/js/semi': 'error',
-    '@stylistic/js/semi-spacing': 'error',
-    '@stylistic/js/space-before-blocks': 'error',
-    '@stylistic/js/space-before-function-paren': ['error', {
+    '@stylistic/operator-linebreak': ['error', 'after'],
+    '@stylistic/padded-blocks': ['error', 'never'],
+    '@stylistic/quote-props': ['error', 'consistent'],
+    '@stylistic/quotes': ['error', 'single', {'allowTemplateLiterals': 'always'}],
+    '@stylistic/semi': 'error',
+    '@stylistic/semi-spacing': 'error',
+    '@stylistic/space-before-blocks': 'error',
+    '@stylistic/space-before-function-paren': ['error', {
       asyncArrow: 'always',
       anonymous: 'never',
       named: 'never',
     }],
-    '@stylistic/js/spaced-comment': ['error', 'always'],
-    '@stylistic/js/switch-colon-spacing': 'error',
+    '@stylistic/spaced-comment': ['error', 'always'],
+    '@stylistic/switch-colon-spacing': 'error',
+
+    // ECMA 2023 rulesets
+    'no-new-native-nonconstructor': 'error',
 
     // ECMA 2022 rulesets
-    '@stylistic/js/arrow-parens': ['error', 'always'],
+    '@stylistic/arrow-parens': ['error', 'always'],
     'constructor-super': 'error',
-    '@stylistic/js/generator-star-spacing': ['error', 'after'],
-    'no-new-symbol': 'error',
+    '@stylistic/generator-star-spacing': ['error', 'after'],
     'no-this-before-super': 'error',
     'no-var': 'error',
     'prefer-const': ['error', {destructuring: 'all'}],
     'prefer-rest-params': 'error',
     'prefer-spread': 'error',
-    'rest-spread-spacing': 'error',
-    'yield-star-spacing': ['error', 'after'],
+    '@stylistic/rest-spread-spacing': 'error',
+    '@stylistic/yield-star-spacing': ['error', 'after'],
   },
 }];
